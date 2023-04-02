e30ab2f0b968625495bd4a892b3256a1d5546f5d
M-pozo <117214588+M-pozo@users.noreply.github.com>
2023-04-02 21:41:56 +0200

    Creando testAceleracion y metodo acelerar

--- src/test/Coche.java
+++ src/test/Coche.java
@@ -3,6 +3,11 @@ package test;
 public class Coche {
 
 	public Object velocidad;
+
+	public void acelerar(int aceleracion) {
+		// TODO Auto-generated method stub
+		velocidad += aceleracion;
+	}
 	
 
 }
--- src/test/CocheTest.java
+++ src/test/CocheTest.java
@@ -8,9 +8,18 @@ import org.junit.jupiter.api.Test;
 class CocheTest {
 
 	@Test
-    public void testCrearCocheVelocidadCeroMiguelPozo(){
+    public void testCrearCocheVelocidadCero(){
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
+	
+	@Test
+    public void testAcelerarCocheVelocidadAumenta(){
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.acelerar(30);
+        Assertions.assertEquals(30, nuevoCoche.velocidad);
+    }
+	
+	
 
 }
